77e090bf0a7ab5b3c3597f31404f29ce4810b1ee
dvish2003 <vishanchathuranga63@gmail.com>
2026-01-29 03:06:34 +0530

    variables define

---
+++ variables/variables.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "Alice"
+	fmt.Println(name)
+
+	var age int = 30
+	fmt.Println(age)
+
+	var isStudent bool = false
+	fmt.Println(isStudent)
+
+
+	var firsNumber int = 30;
+	var secondNumber int = 20;
+	var sum int = firsNumber + secondNumber;
+	fmt.Println("Sum:", sum)
+
+
+
+	var userScore float32 = 95.5 // Example of float32 variable
+	var userBalance float64 = 1500.75// Example of float64 variable
+
+	fmt.Println("User Score:", userScore)
+	fmt.Println("User Balance:", userBalance)
+
+	//print type
+	fmt.Printf("Type of userScore: %T\n", userScore)
+	fmt.Printf("Type of userBalance: %T\n", userBalance)
+	fmt.Printf("Type of name: %T\n", name)
+	
+}
